acc811e0ee30e21452ad42ed7632532d6c7c642e
Nikiro-Ja <143513350+Nikiro-Ja@users.noreply.github.com>
2024-07-05 14:18:38 +0300

    Edit text

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ function getComputerChoice() {
 
 function getHumanChoice() {
 
-    let humanChoice = prompt(`Do you SCISSOR? Do you ROCK? Or are you paper?`).toLowerCase();
+    let humanChoice = prompt(`Do you SCISSORs? Do you ROCK? Or are you paper?`).toLowerCase();
 
     if (humanChoice === `scissors` || humanChoice === `rock` || humanChoice === `paper`) {
         return humanChoice;
-    } else return console.log(`The game is rock-scissors-paper!`) 
+    } else return console.log(`The game is ROCK-PAPER-SCISSORS!`) 
 }
 
 
