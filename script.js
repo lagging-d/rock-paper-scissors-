1eebf9bd214c9902dbc9ce8768dd27bd91649f6f
Nikiro-Ja <143513350+Nikiro-Ja@users.noreply.github.com>
2024-07-24 13:00:36 +0300

    Write switch statement for event delegation

--- script.js
+++ script.js
@@ -8,7 +8,14 @@ btnContainer.addEventListener(`click`, (event) => {
 
     switch(target.id) {
         case `rock`:
-            playRound(`rock`, getComputerChoice())
+            playRound(`rock`, getComputerChoice());
+            break;
+        case `paper`:
+            playRound(`paper`, getComputerChoice());
+            break;
+        case `scissors`:
+            playRound(`scissors`, getComputerChoice());
+            break;
     }
 })
 
